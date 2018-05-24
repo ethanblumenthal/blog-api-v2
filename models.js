b2f3eb659c0da7aac85f4e2e952d1ebda2f162aa
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-05-24 08:29:17 -0500

    added method and virtual to models.js

--- models.js
+++ models.js
@@ -5,12 +5,29 @@ const mongoose = require('mongoose');
 // SCHEMA
 const postSchema = mongoose.Schema({
     title: {type: String, required: true},
-    content: {type: String, required: true},
+    content: {type: String},
+    created: {type: Date, default: Date.now},
     author: {
-      firstName: {type: String, required: true},
-      lastName: {type: String, required: true}
+      firstName: {type: String},
+      lastName: {type: String}
     }
 });
+
+// VIRTUAL PROPERTIES
+postSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim()
+});
+
+// INSTANCE METHODS
+postSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    created: this.created,
+    author: this.authorName,
+  };
+}
 
 const Post = mongoose.model('Post', postSchema);
 
