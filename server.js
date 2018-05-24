7628dd8369a7c75c981ab642acf6eba54a8b84fa
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-05-24 08:48:05 -0500

    PUT request

--- server.js
+++ server.js
@@ -63,6 +63,31 @@ app.post('/posts', (req, res) => {
     });
 });
 
+// PUT REQUEST
+app.put('/posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
+  }
+
+  const updated = {};
+  const updateableFields = ['title', 'content', 'author'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updated[field] = req.body[field];
+    }
+  });
+
+  Post
+    .findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
+    .then(updatedPost => res.status(204).end())
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({error: 'Internal server error'});
+    });
+});
+
 // START AND STOP SERVER
 let server;
 
