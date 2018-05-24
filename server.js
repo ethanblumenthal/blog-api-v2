83d8e600a43ddad680c97de04a3e19ff333ec003
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-05-24 08:07:47 -0500

    setup server.js

---
+++ server.js
@@ -0,0 +1,55 @@
+// IMPORT MODULES
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+const {POST} = require('./models');
+
+const app = express();
+app.use(express.json());
+
+
+// START AND STOP SERVER
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.log(err));
+}
+
+module.exports = {app, runServer, closeServer};
