29d493f22ff8cf02c524c06511042a2a470afca7
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-05-24 07:53:10 -0500

    added variables to config file

---
+++ config.js
@@ -0,0 +1,4 @@
+'use strict';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-api-v2';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blog-api-v2';
+exports.PORT = process.env.PORT | 8080;
